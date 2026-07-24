5fe2f09d1e0373e5e11aadadb7eee42aa5a8bfe8
kingofoc <kingofoc202@gmail.com>
2026-07-24 11:55:03 +0100

    another check

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -14,7 +14,7 @@ export default function Footer() {
  ]
  return (
   <div className="fixed bottom-0 left-0 w-full px-10 py-3">
-   <div className="grid grid-cols-3 gap-8 border-2 border-amber-300 rounded-full p-1">
+   <div className="grid grid-cols-3 gap-8 border-2 tg-secondary-bg rounded-full p-1">
     {footerItems.map(({ href, label, icon }) => {
      const isActive = pathname === href
 
@@ -25,7 +25,7 @@ export default function Footer() {
        className={`flex flex-col justify-center items-center text-center ${isActive ? 'tg-link scale-105 rounded-full' : 'bg-transparent'} transition-all duration-200 ease-in-out`}>
 
         <Image src={icon} width={24} height={24} alt={label} />
-        <span className="tg-text">{label}</span>
+        <span className="tg-text text-xs font-medium">{label}</span>
        
        </Link>
      )
