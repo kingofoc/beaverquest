cda62daa87fa11b4c720825ca5d79e9796f12dd7
kingofoc <kingofoc202@gmail.com>
2026-07-24 12:50:35 +0100

    another commit

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -12,8 +12,8 @@ export default function Footer() {
   { href: "/profile", label: "Me", icon: "/profile.svg" }
  ]
  return (
-  <div className="fixed bottom-0 left-0 w-full px-10 py-3">
-   <div className="grid grid-cols-3 gap-8 secondary-bg rounded-full p-1 shadow-gray-500">
+  <div className="fixed bottom-1 left-0 w-full flex justify-center items-center">
+   <div className="grid grid-cols-3 gap-8 secondary-bg rounded-full p-2">
     {footerItems.map(({ href, label, icon }) => {
      const isActive = pathname === href
 
@@ -21,7 +21,7 @@ export default function Footer() {
       <Link 
        key={label} 
        href={href} 
-       className={`flex flex-col justify-center items-center text-center text-xs ${isActive ? 'tertiary-bg opacity4 scale-105 rounded-full font-bold' : 'bg-transparent text font-medium'} transition-all duration-200 ease-in-out`}>
+       className={`flex flex-col justify-center items-center text-center text-xs p-1 ${isActive ? 'tertiary-bg opacity2 scale-105 rounded-full font-bold' : 'bg-transparent text font-medium'} transition-all duration-200 ease-in-out`}>
 
         <Image src={icon} width={32} height={32} alt={label} />
         <span className="">{label}</span>
