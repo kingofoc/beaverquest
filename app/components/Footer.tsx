e784b3266d459a277482a0335c5c762c9c867fdf
kingofoc <kingofoc202@gmail.com>
2026-07-29 20:39:17 +0100

    adjust padding and footer transition

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -13,7 +13,7 @@ export default function Footer() {
  ]
  return (
   <div className="grid grid-cols-5 fixed bottom-0 left-0 w-full">
-   <div className="grid grid-cols-3 col-span-3 col-start-2 primary-bg rounded-full p-1 mb-8">
+   <div className="grid grid-cols-3 col-span-3 col-start-2 primary-bg rounded-full p-1.5 mb-8">
     {footerItems.map(({ href, label, activeIcon, icon }) => {
      const isActive = pathname === href
 
@@ -21,7 +21,7 @@ export default function Footer() {
       <Link 
        key={label} 
        href={href} 
-       className={`flex flex-col justify-center items-center text-center p-1 ${isActive ? 'tertiary-bg-faded scale-105 rounded-full' : 'bg-transparent'} transition-all duration-200 ease-in-out`}>
+       className={`flex flex-col justify-center items-center text-center p-1 ${isActive ? 'tertiary-bg-faded scale-105 rounded-full' : 'bg-transparent'} transition duration-200 ease-in-out`}>
 
         <TgIcon src={isActive ? activeIcon : icon} size={28} className={isActive ? "icon-color-active" : "icon-color"} />
         <span className={`text-xs ${isActive ? 'font-bold icon-text-color-active' : 'icon-text-color font-medium'}`}>{label}</span>
