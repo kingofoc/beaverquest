e2e1a1cd959e45215c1b1d4d1e5c98fb1327d751
kingofoc <kingofoc202@gmail.com>
2026-07-29 15:43:36 +0100

    use icon component

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,5 @@
 'use client';
 import Link from "next/link";
-import Image from "next/image";
 import { usePathname } from "next/navigation";
 import TgIcon from "./Icon";
 
@@ -8,26 +7,23 @@ export default function Footer() {
  const pathname = usePathname();
 
  const footerItems = [
-  { href: "/publish", label: "Create", icon: "/create.svg" },
-  { href: "/", label: "Tasks", icon: "/taskactive.svg" },
-  { href: "/profile", label: "Me", icon: "/profile.svg" }
+  { href: "/publish", label: "Create", icon: "/create.svg", activeIcon: "/create-active.svg" },
+  { href: "/", label: "Tasks", icon: "/task.svg", activeIcon: "/task-active.svg" },
+  { href: "/profile", label: "Me", icon: "/profile.svg", activeIcon: "/profile-active.svg" }
  ]
  return (
   <div className="grid grid-cols-5 fixed bottom-0 left-0 w-full">
    <div className="grid grid-cols-3 col-span-3 col-start-2 primary-bg rounded-full p-1 mb-8">
-    {footerItems.map(({ href, label, icon }) => {
+    {footerItems.map(({ href, label, activeIcon, icon }) => {
      const isActive = pathname === href
 
      return (
       <Link 
        key={label} 
        href={href} 
-       className={`flex flex-col justify-center items-center text-center text-xs p-1 ${isActive ? 'tertiary-bg-faded scale-105 rounded-full font-bold icon-color-active' : 'bg-transparent font-medium'} transition-all duration-200 ease-in-out`}>
+       className={`flex flex-col justify-center items-center text-center text-xs p-1 ${isActive ? 'tertiary-bg-faded scale-105 rounded-full font-bold link-color' : 'bg-transparent font-medium'} transition-all duration-200 ease-in-out`}>
 
-        <Image src={icon} width={28} height={28} alt={label} />
-        <TgIcon 
-          src={icon} className={isActive ? "icon-color-active" : "icon-color"}
-        />
+        <TgIcon src={isActive ? activeIcon : icon} colorVar={isActive ? "--tg-button-color" : "--tg-text-color"}/>
         <span className="">{label}</span>
        
       </Link>
--- app/components/Icon.tsx
+++ app/components/Icon.tsx
@@ -1,16 +1,27 @@
 'use client';
 
-export default function TgIcon({ src = '', size = 28, className = '' }) {
+type TgIconProps = {
+  src: string;
+  size?: number;
+  colorVar?: string;
+  className?: string;
+};
+
+export default function TgIcon({ src, size = 28, colorVar = "--tg-text-color", className = "" }: TgIconProps) {
  return (
   <div
    className={className}
    style={{
     width: size,
     height: size,
+    display: "inline-block",
+    flexShrink: 0,
+    backgroundColor: `var(${colorVar})`,
     maskImage: `url(${src})`,
     maskSize: 'contain',
     maskRepeat: 'no-repeat',
     maskPosition: 'center',
+    transition: 'background-color 0.2 ease-in-out',
    }}
   />
  )
