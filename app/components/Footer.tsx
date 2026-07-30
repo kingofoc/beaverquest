4aace07c5619d8fd379f64a41e8ffdb23b5cfcff
kingofoc <kingofoc202@gmail.com>
2026-07-30 20:46:30 +0100

    seperate profile

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,19 +2,27 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import TgIcon from "./Icon";
+import { useUser } from "@/context/UserContext";
+import Image from "next/image";
 
 export default function Footer() {
  const pathname = usePathname();
+ const { user } = useUser();
+ const profileUrl = user?.profileURL
 
  const footerItems = [
   { href: "/publish", label: "Create", icon: "/create.svg", activeIcon: "/create-active.svg" },
   { href: "/", label: "Tasks", icon: "/task.svg", activeIcon: "/task-active.svg" },
   { href: "/ads", label: "Ads", icon: "/ads.svg", activeIcon: "/ads-active.svg" },
-  { href: "/profile", label: "Me", icon: "/profile.svg", activeIcon: "/profile-active.svg" }
  ]
+
+ const profileItems = { href: "/profile", label: "Me", icon: "/profile.svg", activeIcon: "/profile-active.svg" }
+
+ const isProfileActive = pathname === profileItems.href;
+
  return (
-  <div className="grid grid-cols-6 fixed bottom-0 left-0 w-full">
-   <div className="grid grid-cols-4 col-span-4 col-start-2 primary-bg rounded-full p-1.5 mb-8">
+  <div className="grid grid-cols-5 fixed bottom-0 left-0 w-full py-2">
+   <div className="grid grid-cols-4 col-span-4 primary-bg rounded-full p-1.5 mb-8">
     {footerItems.map(({ href, label, activeIcon, icon }) => {
      const isActive = pathname === href
      return (
@@ -29,6 +37,27 @@ export default function Footer() {
      )
     })}
 
+   </div>
+
+   <div className="col-span-1 flex justify-center items-center rounded-full">
+    <Link 
+      href={profileItems.href}
+      className={`flex flex-col justify-center items-center rounded-full transition-all duration-500 ${isProfileActive ? "border-2 border-amber-400" : ""}`}>
+        {profileUrl ? (
+          <Image
+            src={profileUrl}
+            alt={"profile"}
+            width={28}
+            height={28}> 
+          </Image>
+        ) : (
+          <TgIcon src={isProfileActive ?  profileItems.activeIcon : profileItems.icon} size={28} className={isProfileActive ? "icon-color-active" : "icon-color"} />
+        )}
+
+        <span className={`text-xs ${isProfileActive ? "font-bold icon-text-color-active" : "font-medium icon-text-color"}`}>
+          {profileItems.label}
+        </span>
+    </Link>
    </div>
   </div>
  )
