f0aa45b15985a9762fd99a0d82977d5a4ecde0a1
kingofoc <kingofoc202@gmail.com>
2026-08-01 12:31:49 +0100

    import profile pic to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import ConnectWallet from "./components/ConnectWallet";
 import Footer from "./components/Footer";
 import HeroCard from "./components/HeroCard";
+import ProfilePics from "./components/ProfilePics";
 import UserCountry from "./components/UserCountry";
 
 export default function Home() {
   return (
     <div>
       <UserCountry />
+      <ProfilePics />
       
       <div>
         <h1>Welcome to BeaverQuest</h1>
