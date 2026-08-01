e7b902b9947fb871932f89216648b05131bd1f55
kingofoc <kingofoc202@gmail.com>
2026-08-01 17:45:29 +0100

    try text color

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -31,7 +31,7 @@ export default function Footer() {
        href={href} 
        className={`flex flex-col justify-center items-center text-center p-1 ${isActive ? 'tertiary-bg-faded scale-105 rounded-full' : 'bg-transparent'} transition duration-500 ease-in-out`}>
 
-        <Image src={isActive ? activeIcon : icon} width={28} height={28} className={isActive ? "icon-color-active" : "icon-color"} alt=""></Image>
+        <Image src={isActive ? activeIcon : icon} width={28} height={28} className={isActive ? "icon-text-color-active" : "icon-color"} alt=""></Image>
         <span className={`text-xs ${isActive ? 'font-bold icon-text-color-active' : 'icon-text-color font-medium'}`}>{label}</span>
       </Link>
      )
