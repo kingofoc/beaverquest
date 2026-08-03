5969901563d599f60afdec0047404f41511db079
kingofoc <kingofoc202@gmail.com>
2026-08-03 09:50:11 +0100

    add animation

--- app/components/HeroCard.tsx
+++ app/components/HeroCard.tsx
@@ -12,8 +12,21 @@ export default function HeroCard() {
    <div className="grid grid-cols-6 gap-4 ">
     <span className="col-span-5 text-2xl font-bold tertiary-text-color">$ {balance}</span>
 
-    <Link href="/boost" className="col-span-1">
-     <Image src="/boost.svg" width={28} height={28} alt=""></Image>
+    <Link href="/boost" className="col-span-1 relative">
+     {[...Array(5)].map((_, i) => (
+      <Image
+       key={i}
+       src="/boost.svg"
+       alt=""
+       width={28}
+       height={28}
+       className="boost-ripple"
+       style={{
+        animationDelay: `${i * 0.4}s`,
+       }}
+      />
+     ))}
+     <Image src="/boost.svg" width={28} height={28} alt="" className="relative z-10"></Image>
     </Link>
    </div>
   </div>
