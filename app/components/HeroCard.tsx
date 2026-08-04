07dad88b8eb441582fc588666b317828e585cb27
kingofoc <kingofoc202@gmail.com>
2026-08-04 16:12:05 +0100

    add gigsgram icon

--- app/components/HeroCard.tsx
+++ app/components/HeroCard.tsx
@@ -2,6 +2,7 @@
 import { useUser } from "@/context/UserContext";
 import Link from "next/link";
 import Image from "next/image";
+import TgIcon from "./Icon";
 
 export default function HeroCard() {
  const { user } = useUser();
@@ -9,8 +10,12 @@ export default function HeroCard() {
  const balance = user?.balance;
  return (
   <div className="w-full gradient-bg p-4 rounded-full">
+   
    <div className="grid grid-cols-6 gap-4 ">
-    <span className="col-span-5 text-2xl font-bold tertiary-text-color"><small className="text-sm font-medium">$</small>{balance}</span>
+    <div className="col-span-5 flex flex-col items-center">
+     <TgIcon src="/gigsgram.svg" size={12} className="tertiary-text-color"/>
+     <span className="text-2xl font-bold tertiary-text-color">{balance}</span>
+    </div>
 
     <Link href="/boost" className="col-span-1 relative">
      {[...Array(5)].map((_, i) => (
