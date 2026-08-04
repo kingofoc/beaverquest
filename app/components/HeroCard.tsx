7fb970ccc5c0a3c6709f4951000aebc6171a5061
kingofoc <kingofoc202@gmail.com>
2026-08-04 17:00:23 +0100

    justify-normal

--- app/components/HeroCard.tsx
+++ app/components/HeroCard.tsx
@@ -11,9 +11,9 @@ export default function HeroCard() {
  return (
   <div className="w-full gradient-bg p-4 rounded-full">
    
-   <div className="grid grid-cols-6 gap-4 ">
-    <div className="col-span-5 flex flex-row justify-center">
-     <TgIcon src="/gigsgram.svg" size={20} className="bg-gray-50"/>
+   <div className="grid grid-cols-6 gap-4">
+    <div className="col-span-5 flex flex-row justify-normal items-center">
+     <TgIcon src="/gigsgram.svg" size={20} className="bg-white"/>
      <span className="text-2xl font-bold tertiary-text-color">{balance}</span>
     </div>
 
