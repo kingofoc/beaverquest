06c2ee1cbb0481c73c7a804f2a8f7bd2f6ea0b0b
kingofoc <kingofoc202@gmail.com>
2026-08-16 19:28:56 +0100

    use justifyend

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -22,7 +22,7 @@ export default function Footer() {
 
  return (
   <div className="fixed bottom-0 left-0 w-full px-8">
-    <div className="primary-bg backdrop-blur-sm grid grid-cols-4 rounded-full mb-8 h-auto">
+    <div className="gradient-bg backdrop-blur-sm grid grid-cols-4 rounded-full mb-8 h-auto">
       <div className="grid grid-cols-3 col-span-3 primary-bg rounded-full p-1.5 w-full">
         {footerItems.map(({ href, label, activeIcon, icon }) => {
         const isActive = pathname === href
@@ -39,7 +39,7 @@ export default function Footer() {
         })}
       </div>
 
-      <div className="col-span-1 flex justify-center items-center rounded-full w-full">
+      <div className="col-span-1 flex justify-end items-center rounded-full w-full">
         <Link href={profileItems.href}
         className={`relative w-16 h-16 rounded-full overflow-hidden transition-all duration-500 ${isProfileActive ? "border-active" : ""}`}>
           {profileUrl ? (
