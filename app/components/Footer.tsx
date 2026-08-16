962108126cee02ceb84087c2a8773b18ef74a508
kingofoc <kingofoc202@gmail.com>
2026-08-16 20:05:08 +0100

    use another blur

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -21,11 +21,9 @@ export default function Footer() {
  const isProfileActive = pathname === profileItems.href;
 
  return (
-  <div className="textiary-bg-faded fixed bottom-0 left-0 w-full px-8">
+  <div className="textiary-bg-faded backdrop-blur-sm fixed bottom-0 left-0 w-full px-8">
     <div className="relative grid grid-cols-4 rounded-full mb-8">
-      <div
-        className="footer-blur-layer"
-      />
+      <div className="absolute bottom-0 z-0 h-2/3 textiary-bg-faded backdrop-blur-sm"></div>
       <div className="relative z-10 grid grid-cols-3 col-span-3 primary-bg rounded-full p-1.5 w-full">
         {footerItems.map(({ href, label, activeIcon, icon }) => {
         const isActive = pathname === href
