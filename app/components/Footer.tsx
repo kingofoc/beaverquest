bfeea27a15a509b6543e910ef891d7129315c654
kingofoc <kingofoc202@gmail.com>
2026-08-17 07:39:24 +0100

    add w-full

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -21,8 +21,8 @@ export default function Footer() {
  const isProfileActive = pathname === profileItems.href;
 
  return (
-  <div className="fixed bottom-0 insect-x-0 z-50">
-    <div className="absolute bottom-0 insect-x-0 z-0 h-32 secondary-bg-faded backdrop-blur-sm mask-t-from-80%"></div>
+  <div className="fixed bottom-0 insect-x-0 z-50 w-full">
+    <div className="absolute bottom-0 insect-x-0 z-0 h-32 w-full secondary-bg-faded backdrop-blur-sm mask-t-from-80%"></div>
     <div className="relative z-10 grid grid-cols-4 mb-8 px-8">
       <div className="grid grid-cols-3 col-span-3 primary-bg rounded-full p-1.5 w-full">
         {footerItems.map(({ href, label, activeIcon, icon }) => {
