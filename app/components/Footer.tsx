dc95059a1c46fe87d695503c4302ab54fb45a54b
kingofoc <kingofoc202@gmail.com>
2026-08-17 05:21:59 +0100

    add relative position

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -22,7 +22,7 @@ export default function Footer() {
 
  return (
   <div className="fixed bottom-0 left-0 w-full px-8">
-    <div className="grid grid-cols-4 mb-8">
+    <div className="relative grid grid-cols-4 mb-8">
       
       <div className="relative z-10 grid grid-cols-3 col-span-3 primary-bg rounded-full p-1.5 w-full">
         {footerItems.map(({ href, label, activeIcon, icon }) => {
@@ -55,7 +55,7 @@ export default function Footer() {
           )}
         </Link>
       </div>
-      <div className="absolute bottom-0 z-0 h-32 secondary-bg-faded backdrop-blur-sm mask-t-from-80% mask-radial-from-70% mask-radial-to-80%"></div>
+      <div className="absolute bottom-0 z-1 h-32 secondary-bg-faded backdrop-blur-sm mask-t-from-80% mask-radial-from-70% mask-radial-to-80%"></div>
     </div>
   </div>
  )
