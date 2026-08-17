4c246d249933aa2eec70a56ff22eaca66c3420c4
kingofoc <kingofoc202@gmail.com>
2026-08-17 08:08:18 +0100

    increase %

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -22,7 +22,7 @@ export default function Footer() {
 
  return (
   <div className="fixed bottom-0 inset-x-0 z-50">
-    <div className="absolute bottom-0 inset-x-0 z-0 h-10 secondary-bg-faded backdrop-blur-sm mask-t-from-60%"></div>
+    <div className="absolute bottom-0 inset-x-0 z-0 h-10 secondary-bg-faded backdrop-blur-sm mask-t-from-75%"></div>
     <div className="relative z-10 grid grid-cols-4 mb-4 px-8">
       <div className="grid grid-cols-3 col-span-3 primary-bg rounded-full p-1.5 w-full">
         {footerItems.map(({ href, label, activeIcon, icon }) => {
