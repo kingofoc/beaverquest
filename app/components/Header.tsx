75f7d833a2c39306b4759497cd1ee118d75caca4
kingofoc <kingofoc202@gmail.com>
2026-08-19 18:44:57 +0100

    reduce header h

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
 
  return (
   <div className="fixed top-0 inset-x-0 z-50">
-   <div className="absolute top-0 inset-x-0 z-0 h-26 secondary-bg-faded backdrop-blur-sm mask-b-from-65%"></div>
+   <div className="absolute top-0 inset-x-0 z-0 h-10 secondary-bg-faded backdrop-blur-sm mask-b-from-65%"></div>
    <div className="relative z-10 grid grid-cols-6 p-4">
     <Link href={profileItems.href} className="flex items-center col-span-3 gap-2">
      <div className="relative w-7 h-7 rounded-full overflow-hidden transition-all duration-500 border-active">
