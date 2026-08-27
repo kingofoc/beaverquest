d4478fb48ef8aefdb2f4b3b9bcbde4902b91b3d2
kingofoc <kingofoc202@gmail.com>
2026-08-27 17:06:31 +0100

    use community in GET request

--- app/api/communities/route.ts
+++ app/api/communities/route.ts
@@ -119,7 +119,7 @@ export async function GET(req: NextRequest) {
 
   const allCommunities = await Community.find({})
    .sort({ memberCount: -1 })
-   .select('channelId channelName channelType memberCount')
+   .select('channelId communityName communityType memberCount')
 
   const topCommunities = allCommunities.slice(0, 100);
 
