78e850f0b18af125f0ea3e7f0f334ee9b57c48f6
kingofoc <kingofoc202@gmail.com>
2026-08-29 20:49:11 +0100

    correct link

--- app/publish/page.tsx
+++ app/publish/page.tsx
@@ -6,15 +6,15 @@ export default function Publish() {
  return (
   <div>
    <div>
-    <h1 className="mb-4 font-bold">Choose Campaign Type</h1>
-    <p>Select the number of impression or clicks you need, create your gig, pay with GIGSGRAM Token</p>
+    <h1 className="mb-2 font-bold text-xl">Choose Campaign Type</h1>
+    <p className="text-sm hint-color">Select the number of impression or clicks you need, create your gig, pay with GIGSGRAM Token</p>
    </div>
    <div className="mt-4">
-    <Link href="/socialGig" className="w-full rounded-lg primary-bg">
+    <Link href="/publish/socialGig" className="w-full rounded-lg secondary-bg">
      <div className="p-4 flex flex-row gap-4 items-center">
-      <TgIcon src="/clicks.svg" size={28}></TgIcon>
-      <div>
-       <span className="text-xl font-medium">Standard Click Task</span>
+      <TgIcon src="/click.svg" size={28} className="icon-color"></TgIcon>
+      <div className="flex flex-col">
+       <span className="text-lg font-medium">Standard Click Task</span>
        <span className="text-sm hint-color">Pay per clicks. Perfect for traffic and engagement.</span>
       </div>
      </div>
